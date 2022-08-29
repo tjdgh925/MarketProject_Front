c589955d588ccd45a1ee7c9ef3447b7069d09764
Sungho Jang <tjdgh925@gmail.com>
2022-08-29 17:35:36 +0900

    feat: 회원가입 화면 구현

---
+++ front/src/components/register/RegisterButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '../common/Button';
+
+const RegisterButton = () => {
+  return <Button width="70%">회원가입</Button>;
+};
+
+export default RegisterButton;
--- front/src/pages/register/RegisterPage.jsx
+++ front/src/pages/register/RegisterPage.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import RegisterButton from '../../components/register/RegisterButton';
+import RegisterForm from '../../components/register/RegisterForm';
+
+const RegisterBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 95%;
+  align-items: center;
+`;
+
+const RegisterBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.2);
+  width: 600px;
+  background: white;
+  border: #e3e3e3 2px solid;
+  padding: 7rem;
+  border-radius: 20px;
+`;
 
 const RegisterPage = () => {
-  return <div>HOME</div>;
+  return (
+    <RegisterBlock>
+      <RegisterBox>
+        <RegisterForm />
+        <RegisterButton />
+      </RegisterBox>
+    </RegisterBlock>
+  );
 };
 
 export default RegisterPage;
