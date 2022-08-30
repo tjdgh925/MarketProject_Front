f0488b5d9ea50b06b991611bb64274f3a348242d
Sungho Jang <tjdgh925@gmail.com>
2022-08-30 17:06:15 +0900

    refactor: 에러 메시지 출력 방법 변경

--- front/src/components/common/ErrorMessage.jsx
+++ front/src/components/common/ErrorMessage.jsx
@@ -2,10 +2,18 @@ import styled from 'styled-components';
 
 const ErrorBlock = styled.h4`
   color: red;
+  text-align: center;
 `;
 
 const ErrorMessage = (prop) => {
-  return <ErrorBlock>{prop.children}</ErrorBlock>;
+  const ErrorArray = prop.children.split('\n');
+  return (
+    <div>
+      {ErrorArray.map((error) => (
+        <ErrorBlock>{error}</ErrorBlock>
+      ))}
+    </div>
+  );
 };
 
 export default ErrorMessage;
